5eed9816be4e17cb7726aa383d020bb2094cc7b2
Spibb20 <Spibb20@gmail.com>
2025-11-05 12:19:28 +0800

    logo components

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Navigation } from "./_components/Navigation";
+import { AdminNavigation } from "./_components/AdminNavigation";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navigation />
+        <AdminNavigation />
         {children}
       </body>
     </html>
