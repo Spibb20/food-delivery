9ce0faaf050b4ffa414bb5299e997a8377542d60
Spibb20 <Spibb20@gmail.com>
2025-11-10 11:42:21 +0800

    user crud, jwtokenized, middleware

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,12 +1,21 @@
 import bcrypt from "bcrypt";
 import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
+const SECRET = process.env.SECRET;
 
 export const signUp = async (req, res) => {
   try {
-    const { body } = req;
-    const { email, password } = body;
+    const { email, password, phoneNumber, address } = req.body;
 
-    const hashedPassword = await bcrypt.hashSync(password, 10);
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).send({ message: "Email already exists" });
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
     console.log("Password", password);
     console.log("Hash password", hashedPassword);
@@ -14,33 +23,63 @@ export const signUp = async (req, res) => {
     const user = await User.create({
       email,
       password: hashedPassword,
+      phoneNumber,
+      address,
     });
 
-    res.status(200).send({ message: "Success", data: user });
+    res.status(201).send({ message: "Success", data: user });
   } catch (error) {
     res.status(500).send({ message: "Error", error });
   }
 };
 
 export const signIn = async (req, res) => {
-  const { body } = req;
-  const { email, password } = body;
   try {
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     console.log("USER", user);
 
-    if (!user.length) {
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
     console.log("Body error", email, password);
 
     const isPasswordCorrect = bcrypt.compareSync(password, user.password);
 
-    if (!isPasswordCorrect) {
+    if (!isPasswordCorrect)
       return res.status(403).send({ message: "Password is incorrect" });
-    } else return res.status(200).send({ message: "Success", data: user });
+
+    const token = jwt.sign({ userId: user._id, role: user.role }, SECRET, {
+      expiresIn: "1d",
+    });
+    return res.status(200).send({ message: "Success", token, user });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: "Server Error" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+
+    res.status(200).send({ message: "User updated", data: updatedUser });
+  } catch {
+    res.status(500).send({ message: "Error" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    await User.findByIdAndDelete(userId);
+    res.status(200).send({ message: "User deleted" });
+  } catch {
+    res.status(500).send({ message: "Error" });
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import { connectDB } from "./database/db.js";
 import categoryRouter from "./routes/foodCategoryRoute.js";
 import { userRouter } from "./routes/userRoute.js";
+import cors from "cors";
 
 configDotenv();
 
@@ -11,6 +12,12 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use("/food-category", categoryRouter);
 app.use("/user", userRouter);
